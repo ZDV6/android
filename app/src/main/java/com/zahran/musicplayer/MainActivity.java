e8f75494421a0b7d6797825f249f4a66cc75f1b9
Zahran <zahranrplb@gmail.com>
2018-11-24 21:42:00 +0700

    Masih ada yang salah

---
+++ app/src/main/java/com/zahran/musicplayer/MainActivity.java
@@ -0,0 +1,25 @@
+package com.zahran.musicplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnSongs = (Button) findViewById(R.id.btnSongs);
+        btnSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
